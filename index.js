0f37022244556598d044060b5853acea38b8bfab
Elon Gliksberg <elongliks@gmail.com>
2023-06-16 19:46:28 +0300

    Initial commit.

---
+++ index.js
@@ -0,0 +1,37 @@
+import { CompanyTypes, createScraper } from "israeli-bank-scrapers";
+
+const main = async () => {
+  const now = new Date();
+  const options = {
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    startDate: new Date(now.getFullYear(), now.getMonth(), 1),
+    companyId: CompanyTypes.isracard,
+    showBrowser: false,
+  };
+
+  const credentials = {
+    id: process.env.CREDENTIALS_ID,
+    card6Digits: process.env.CREDENTIALS_CARD_DIGITS,
+    password: process.env.CREDENTIALS_PASSWORD,
+  };
+
+  const scraper = createScraper(options);
+  const scrapeResult = await scraper.scrape(credentials);
+
+  if (!scrapeResult.success) {
+    throw new Error(
+      `Failed to scrape: ${scrapeResult.errorType}: ${scrapeResult.errorMessage}`
+    );
+  }
+
+  let totalCharged = 0;
+  scrapeResult.accounts.forEach((account) => {
+    account.txns.forEach((txn) => {
+      // The charge amount is negative.
+      totalCharged -= txn.chargedAmount;
+    });
+  });
+  console.log(`Total charged: ${totalCharged}`);
+};
+
+main();
