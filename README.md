1dbbfb50582ba81e3738470eb09566c66a95b56a
Elon Gliksberg <elongliks@gmail.com>
2023-06-16 20:05:32 +0300

    Updated code.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# Setup
+# Budget Notifier
+
+A simple program that notifies you when you exceed your budget.
+_Note_: This program is currently only compatible with Isracard.
+
+## Setup
 
 1. Create the following `.env` file:
 
@@ -9,6 +14,8 @@ PUPPETEER_EXECUTABLE_PATH=<chromium path>
 CREDENTIALS_ID=<user identification number>
 CREDENTIALS_CARD_DIGITS=<last 6 digits of card>
 CREDENTIALS_PASSWORD=<password>
+
+BUDGET_ILS=<budget in ILS>
 ```
 
 2. Install the package
--- index.js
+++ index.js
@@ -3,7 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const main = async () => {
+const parseBudget = () => {
+  const budget = Number(process.env.BUDGET_ILS);
+  if (isNaN(budget)) {
+    throw new Error("Budget is not a number");
+  }
+  return budget;
+};
+
+const scrape = async () => {
   const now = new Date();
   const options = {
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
@@ -26,7 +34,10 @@ const main = async () => {
       `Failed to scrape: ${scrapeResult.errorType}: ${scrapeResult.errorMessage}`
     );
   }
+  return scrapeResult;
+};
 
+const getTotalCharged = (scrapeResult) => {
   let totalCharged = 0;
   scrapeResult.accounts.forEach((account) => {
     account.txns.forEach((txn) => {
@@ -34,7 +45,18 @@ const main = async () => {
       totalCharged -= txn.chargedAmount;
     });
   });
-  console.log(`Total charged: ${totalCharged}`);
+  return totalCharged;
+};
+
+const main = async () => {
+  const budget = parseBudget();
+  const scrapeResult = await scrape();
+  const totalCharged = getTotalCharged(scrapeResult);
+  if (totalCharged > budget) {
+    console.log(`Budget exceeded: ${totalCharged}`);
+  } else {
+    console.log(`Total charged: ${totalCharged}`);
+  }
 };
 
 main();
