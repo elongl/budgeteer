16dd8f81668a5e38acc72dfd1e60bb4ac6a62e26
Elon Gliksberg <elongliks@gmail.com>
2024-01-26 11:31:05 +0200

    Formatted the changes.

--- index.js
+++ index.js
@@ -41,84 +41,96 @@ const scrape = async (startDate) => {
 const getTotalCharged = (scrapeResult) => {
   let totalCharged = {};
   let monthYearTuple = "";
-  
+
   scrapeResult.accounts.forEach((account) => {
     account.txns.forEach((txn) => {
       monthYearTuple = getMonthYearTupleOfADay(new Date(txn.date));
       if (!(monthYearTuple in totalCharged)) {
-        totalCharged[monthYearTuple] = 0
+        totalCharged[monthYearTuple] = 0;
       }
-      
+
       // The charge amount is negative.
       totalCharged[monthYearTuple] -= txn.chargedAmount;
     });
   });
 
-  Object.keys(totalCharged).forEach(monthYear => {
+  Object.keys(totalCharged).forEach((monthYear) => {
     totalCharged[monthYear] = Math.round(totalCharged[monthYear] * 100) / 100;
   });
-  
-  return totalCharged
+
+  return totalCharged;
 };
 
 const getPreviousMonthDate = (numberOfMonthsBack) => {
-    let previousMonthDate = new Date();
+  let previousMonthDate = new Date();
+  previousMonthDate.setDate(1);
+  for (let index = 0; index < numberOfMonthsBack; index++) {
+    previousMonthDate.setDate(0);
     previousMonthDate.setDate(1);
-    for (let index = 0; index < numberOfMonthsBack; index++) {
-        previousMonthDate.setDate(0);
-        previousMonthDate.setDate(1);
-    }
-    return previousMonthDate;
-}
+  }
+  return previousMonthDate;
+};
 
 const createMessageForCurrentMonth = (currentMonthCharges, budget) => {
   let message;
   const exceed_percent = Math.round((currentMonthCharges / budget) * 100);
-  
+
   if (currentMonthCharges > budget) {
     message = `Current month budget exceeded: ${currentMonthCharges} > ${budget} (${exceed_percent}%)`;
   } else {
     message = `Current month budget not exceeded: ${currentMonthCharges} <= ${budget} (${exceed_percent}%)`;
   }
-  
+
   return message;
-}
+};
 
 const createMessageForPreviousMonths = (previousCharges, budget) => {
   let message;
-  const totalCharges = previousCharges.reduce((partialSum, a) => partialSum + a, 0);
+  const totalCharges = previousCharges.reduce(
+    (partialSum, a) => partialSum + a,
+    0
+  );
   const exceed_percent = Math.round((totalCharged / budget) * 100);
-  
-  if (totalCharges > budget * (previousCharges.length) ) {
+
+  if (totalCharges > budget * previousCharges.length) {
     message = `Over the last ${previousCharges.length} months budget exceeded: ${totalCharged} > ${budget} (${exceed_percent}%)`;
   } else {
     message = `Over the last ${previousCharges.length} budget not exceeded: ${totalCharged} <= ${budget} (${exceed_percent}%)`;
   }
-  
+
   return message;
-}
+};
 
 const getMonthYearTupleOfADay = (day) => {
-  monthYearTuple = `${day.toLocaleString("en-US", {month:"short"})}-${day.getFullYear()}`
-}
+  monthYearTuple = `${day.toLocaleString("en-US", {
+    month: "short",
+  })}-${day.getFullYear()}`;
+};
 
 const main = async () => {
   let message;
-  const monthsBack = process.env.MONTHS_BACK ? Number(process.env.MONTHS_BACK) : 0;
+  const monthsBack = process.env.MONTHS_BACK
+    ? Number(process.env.MONTHS_BACK)
+    : 0;
   const budget = parseBudget();
   const scrapeResult = await scrape(getPreviousMonthDate(monthsBack));
   const totalCharged = getTotalCharged(scrapeResult);
   const destinations = process.env.DESTINATIONS.split(",");
 
-  const todayMonthYearTuple = getMonthYearTupleOfADay(now)
+  const todayMonthYearTuple = getMonthYearTupleOfADay(now);
   if (todayMonthYearTuple in totalCharged) {
-    message = `${createMessageForCurrentMonth(totalCharged[todayMonthYearTuple], budget)}\n`
+    message = `${createMessageForCurrentMonth(
+      totalCharged[todayMonthYearTuple],
+      budget
+    )}\n`;
   } else {
-    message = "There are no charges for this month yet :) \n"
+    message = "There are no charges for this month yet :) \n";
   }
 
-  message += monthsBack ? createMessageForPreviousMonths(Object.keys(totalCharged), budget) : ""
-  
+  message += monthsBack
+    ? createMessageForPreviousMonths(Object.keys(totalCharged), budget)
+    : "";
+
   destinations.forEach((dest) => {
     if (dest === "TELEGRAM") {
       sendTelegramMessage(message);
